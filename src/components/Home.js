adbfb4f710da78e85fc81ceaaf7d29edc45aae23
Gurjinder Singh <rockygsingh@icloud.com>
2022-05-02 03:01:40 -0400

    created navbar and rendered in the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Navbar from './Navbar';
 
 class Home extends React.Component {
 
     render() {
         return (
             <div>
+                <Navbar />
                 <h1>This is the home page!</h1>
             </div>
         )
